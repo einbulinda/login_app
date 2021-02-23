b33f51ac0a29b9bc73b475a6c1f4940628b6e1d2
einbulinda <einbulinda@gmail.com>
2021-02-23 15:08:01 +0000

    nav styled, reg page introduced

--- app.js
+++ app.js
@@ -7,8 +7,15 @@ var app = express();
 // set view engine to ejs
 app.set('view engine','ejs');
 
+// Serving static files for website
+app.use(express.static('public'))
+
 app.get('/',(req,res)=>{
     res.render('index');
+})
+
+app.get('/register',(req,res)=>{
+    res.render('register');
 })
 
 
