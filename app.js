0cc1ccf7517ce5fa98a77cbb30e70a46b66646c7
einbulinda <einbulinda@gmail.com>
2021-02-24 00:19:55 +0300

    DB Connection made

--- app.js
+++ app.js
@@ -1,28 +1,45 @@
-var express = require('express');
-var ejs = require('ejs');
+var express = require("express");
+var ejs = require("ejs");
+var mysql = require("mysql");
+const dotenv = require("dotenv");
 
 // Creating a server with express module.
 var app = express();
 
+dotenv.config({ path: "./.env" });
+
 // set view engine to ejs
-app.set('view engine','ejs');
+app.set("view engine", "ejs");
 
 // Serving static files for website
-app.use(express.static('public'))
+app.use(express.static("public"));
 
-app.get('/',(req,res)=>{
-    res.render('index');
-})
+// DB Connection details
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DATABASE,
+});
 
-app.get('/register',(req,res)=>{
-    res.render('register');
-})
+// Connecting to DB
+db.connect((error) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Database Connected...");
+  }
+});
 
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
-
-
+app.get("/register", (req, res) => {
+  res.render("register");
+});
 
 const PORT = 3000;
-app.listen(PORT, ()=>{
-    console.log(`Server is running on port: ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}`);
+});
